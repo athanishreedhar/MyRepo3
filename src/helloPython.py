d24cdc0a198eb2644c098b8d4f55301089607245
athanishreedhar <shridhara1213@gmail.com>
2020-08-07 22:02:43 +0530

    sixth comment by Python shreedhar

--- src/helloPython.py
+++ src/helloPython.py
@@ -3,7 +3,6 @@ Created on Mar 13, 2020
 
 @author: user
 '''
-print("hello python world")
-print ("python")
-print ("python")
-print ("python")
+
+print("===================Hello Hello=================")
+print("===================Hello Hello=================")
